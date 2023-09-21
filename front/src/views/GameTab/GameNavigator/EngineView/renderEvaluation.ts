4eedb65da325d0a68ac98362b8b2b44ac75c19d0
Kévin Michelet <kevinmichelet91@gmail.com>
2023-09-21 16:48:45 +0200

    fix display mate value

--- front/src/views/GameTab/GameNavigator/EngineView/renderEvaluation.ts
+++ front/src/views/GameTab/GameNavigator/EngineView/renderEvaluation.ts
@@ -1,7 +1,7 @@
 import { EngineData } from '../../../../types/types';
 
 export default function renderEvaluation(playerToMove: 'w' | 'b', engineData: EngineData) {
-    if (engineData.score.unit === 'mate') return 'M' + engineData.score.value;
+    if (engineData.score.unit === 'mate') return 'M' + Math.abs(engineData.score.value);
     else if (engineData.score.unit === 'cp') {
         //UCI specifies the evaluation from the player's point of view
         let sign = '';
